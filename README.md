1ed4c2b27a0d812f8b814b34b4834a1ef69c90fd
SongeBobSun <widekuan@outlook.com>
2015-05-24 20:39:58 +0800

    Initial commit.

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Contains CustomView template and some enhanced Base Views.
 Component may and will be used in my further projects.
 Every time I finished a component, below index will be updated.
 ## Index of Content
-### Not a single ghost.
+### SBView
+Provide a Base View for customize widgets. SBView can calculate View's width & height.
+You can use getWidth() & getHeight() directly in onDraw() function.
+
 
 #SB means 'Some Base', 'Sun.Bob' or 'Dumbass' in Chinese.
---
+++ sblib/src/main/java/sblib/bobsun/sblib/Widgets/SBView.java
@@ -0,0 +1,47 @@
+package sblib.bobsun.sblib.Widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by bobsun on 15-5-24.
+ */
+public class SBView extends View {
+
+    public SBView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int measureWidthSpec,int measureHeightSpec){
+        super.onMeasure(measureWidthSpec,measureHeightSpec);
+        int measuredWidth = measureWidth(measureWidthSpec);
+        int measuredHeight = measureHeight(measureHeightSpec);
+        this.setMeasuredDimension(measuredWidth,measuredHeight);
+    }
+
+    private int measureWidth(int measureSpec) {
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        int result = 0;
+        if (specMode == MeasureSpec.AT_MOST) {
+            result = getWidth();
+        } else if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        }
+        return result;
+    }
+    private int measureHeight(int measureSpec) {
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        int result = 0;
+        if (specMode == MeasureSpec.AT_MOST) {
+            result = getWidth();
+        } else if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        }
+        return result;
+    }
+
+}
